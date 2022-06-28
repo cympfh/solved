7f6acc13cd87f600eb7242a6298f08b2fda169f5
cympfh <cympfh@gmail.com>
2022-06-28 22:48:39 +0900

    ABC257; Virtual

---
+++ atcoder/abc257/src/bin/c.rs
@@ -0,0 +1,190 @@
+#![allow(unused_imports, unused_macros, dead_code)]
+use std::{cmp::*, collections::*};
+
+fn main() {
+    let mut sc = Scanner::new();
+    let n: usize = sc.cin();
+    let is_adult: Vec<bool> = sc.chars().into_iter().map(|c| c == '1').collect();
+    let ws: Vec<i64> = sc.vec(n);
+
+    let mut children = vec![];
+    let mut adults = vec![];
+    for i in 0..n {
+        if is_adult[i] {
+            adults.push(ws[i]);
+        } else {
+            children.push(ws[i]);
+        }
+    }
+    children.sort();
+    adults.sort();
+
+    let mut cans = ws.clone();
+    cans.push(0);
+    cans.push(1_000_000_010);
+
+    let mut ans = 0;
+    for w in cans {
+        let a1 = count(&children, 0..w);
+        let a2 = count(&adults, w..1_000_000_010);
+        let acc = a1 + a2;
+        ans = max!(ans, acc);
+    }
+    put!(ans);
+}
+
+// @algorithm/binary_search_count
+// @algorithm/binary_search
+/// Algorithm - Binary Search (lowerbound)
+pub trait CompleteIdx: Copy {
+    fn mid(self, other: Self) -> Self;
+}
+#[macro_export]
+macro_rules! completeidx {
+    ( $type:ty, mid($self:ident, $other:ident) = $code:block ) => {
+        impl CompleteIdx for $type {
+            fn mid($self, $other: Self) -> Self { $code }
+        }
+    };
+}
+completeidx! { usize, mid(self, other) = { (self + other) / 2 }}
+completeidx! { u128, mid(self, other) = { (self + other) / 2 }}
+completeidx! { i128, mid(self, other) = { (self + other) / 2 }}
+completeidx! { u64, mid(self, other) = { (self + other) / 2 }}
+completeidx! { i64, mid(self, other) = { (self + other) / 2 }}
+completeidx! { f64, mid(self, other) = { (self + other) / 2.0 }}
+
+pub fn lowerbound<T: CompleteIdx>(r: std::ops::Range<T>, cond: &dyn Fn(T) -> bool) -> Option<T> {
+    if cond(r.start) {
+        return Some(r.start);
+    }
+    // TODO(from 1.47.0)
+    // if r.is_empty() { return None }
+    let mut left = r.start;
+    let mut right = r.end;
+    let mut ok = false;
+    for _ in 0..100 {
+        let mid = T::mid(left, right);
+        if cond(mid) {
+            right = mid;
+            ok = true;
+        } else {
+            left = mid;
+        }
+    }
+    if ok {
+        Some(right)
+    } else {
+        None
+    }
+}
+
+/// num of elements in the range
+pub fn count<X: CompleteIdx + Ord>(xs: &Vec<X>, range: std::ops::Range<X>) -> usize {
+    let n = xs.len();
+    if n == 0 {
+        return 0;
+    }
+    let rightout = lowerbound(0..n, &|i| xs[i] >= range.end)
+        .map(|i| n - i)
+        .unwrap_or(0);
+    let rightin = lowerbound(0..n, &|i| xs[i] >= range.start)
+        .map(|i| n - i)
+        .unwrap_or(rightout);
+    rightin - rightout
+}
+
+// {{{
+use std::io::{self, Write};
+use std::str::FromStr;
+
+pub struct Scanner {
+    stdin: io::Stdin,
+    buffer: VecDeque<String>,
+}
+impl Scanner {
+    pub fn new() -> Self {
+        Self {
+            stdin: io::stdin(),
+            buffer: VecDeque::new(),
+        }
+    }
+    pub fn cin<T: FromStr>(&mut self) -> T {
+        while self.buffer.is_empty() {
+            let mut line = String::new();
+            let _ = self.stdin.read_line(&mut line);
+            for w in line.split_whitespace() {
+                self.buffer.push_back(String::from(w));
+            }
+        }
+        self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap()
+    }
+    pub fn usize1(&mut self) -> usize {
+        self.cin::<usize>() - 1
+    }
+    pub fn chars(&mut self) -> Vec<char> {
+        self.cin::<String>().chars().collect()
+    }
+    pub fn vec<T: FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.cin()).collect()
+    }
+}
+fn flush() {
+    std::io::stdout().flush().unwrap();
+}
+#[macro_export]
+macro_rules! min {
+    (.. $x:expr) => {{
+        let mut it = $x.iter();
+        it.next().map(|z| it.fold(z, |x, y| min!(x, y)))
+    }};
+    ($x:expr) => ($x);
+    ($x:expr, $($ys:expr),*) => {{
+        let t = min!($($ys),*);
+        if $x < t { $x } else { t }
+    }}
+}
+#[macro_export]
+macro_rules! max {
+    (.. $x:expr) => {{
+        let mut it = $x.iter();
+        it.next().map(|z| it.fold(z, |x, y| max!(x, y)))
+    }};
+    ($x:expr) => ($x);
+    ($x:expr, $($ys:expr),*) => {{
+        let t = max!($($ys),*);
+        if $x > t { $x } else { t }
+    }}
+}
+#[macro_export]
+macro_rules! trace {
+    ($x:expr) => {
+        #[cfg(debug_assertions)]
+        eprintln!(">>> {} = {:?}", stringify!($x), $x)
+    };
+    ($($xs:expr),*) => { trace!(($($xs),*)) }
+}
+#[macro_export]
+macro_rules! put {
+    (yes) => {println!("Yes")}; (no) => {println!("No")};
+    (# $a:ident) => {println!("{}", stringify!($a))};
+    (.. $x:expr) => {{
+        let mut it = $x.iter();
+        if let Some(x) = it.next() { print!("{}", x); }
+        for x in it { print!(" {}", x); }
+        println!("");
+    }};
+    ($x:expr) => { println!("{}", $x) };
+    ($x:expr, $($xs:expr),*) => { print!("{} ", $x); put!($($xs),*) }
+}
+#[macro_export]
+macro_rules! ndarray {
+    ($x:expr;) => {
+        $x
+    };
+    ($x:expr; $size:expr $( , $rest:expr )*) => {
+        vec![ndarray!($x; $($rest),*); $size]
+    };
+}
+
+// }}}
