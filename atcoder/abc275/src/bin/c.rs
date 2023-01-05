ed8f9bfab5640ddceb054e679d77c4c9e085f886
cympfh <cympfh@gmail.com>
2023-01-05 19:21:20 +0900

    ABC275; Virtual

---
+++ atcoder/abc275/src/bin/c.rs
@@ -0,0 +1,146 @@
+#![allow(unused_imports, unused_macros, dead_code)]
+use std::{cmp::*, collections::*};
+
+/// Array-indexing by i64.
+/// (Vec<Vec<..<T>..>>; i64, i64, ...) => Option<T>
+macro_rules! at {
+    ($s:expr;) => { Some($s) };
+    ($s:expr; $idx:expr $( , $args:expr)* $(,)?) => {
+        if 0 <= $idx {
+            let idx_usize = $idx as usize;
+            if idx_usize < $s.len() {
+                at!($s[idx_usize]; $($args),*)
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let s: Vec<Vec<char>> = (0..9).map(|_| sc.chars()).collect();
+    let mut ans = 0;
+    for x in 0..9_i64 {
+        for y in 0..9_i64 {
+            for dx in -9..9 {
+                for dy in -9..9 {
+                    if dx == 0 && dy == 0 {
+                        continue;
+                    }
+                    let c0 = at!(s; x, y);
+                    let c1 = at!(s; x + dx, y + dy);
+                    let c2 = at!(s; x + dx - dy, y + dy + dx);
+                    let c3 = at!(s; x - dy, y + dx);
+
+                    match (c0, c1, c2, c3) {
+                        (Some(c0), Some(c1), Some(c2), Some(c3))
+                            if c0 == '#' && c1 == '#' && c2 == '#' && c3 == '#' =>
+                        {
+                            ans += 1;
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
+    }
+    put!(ans / 4);
+}
+
+// {{{
+use std::io::{self, Write};
+use std::str::FromStr;
+
+pub struct Scanner {
+    stdin: io::Stdin,
+    buffer: VecDeque<String>,
+}
+impl Scanner {
+    pub fn new() -> Self {
+        Self {
+            stdin: io::stdin(),
+            buffer: VecDeque::new(),
+        }
+    }
+    pub fn cin<T: FromStr>(&mut self) -> T {
+        while self.buffer.is_empty() {
+            let mut line = String::new();
+            let _ = self.stdin.read_line(&mut line);
+            for w in line.split_whitespace() {
+                self.buffer.push_back(String::from(w));
+            }
+        }
+        self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap()
+    }
+    pub fn usize1(&mut self) -> usize {
+        self.cin::<usize>() - 1
+    }
+    pub fn chars(&mut self) -> Vec<char> {
+        self.cin::<String>().chars().collect()
+    }
+    pub fn vec<T: FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.cin()).collect()
+    }
+}
+fn flush() {
+    std::io::stdout().flush().unwrap();
+}
+#[macro_export]
+macro_rules! min {
+    (.. $x:expr) => {{
+        let mut it = $x.iter();
+        it.next().map(|z| it.fold(z, |x, y| min!(x, y)))
+    }};
+    ($x:expr) => ($x);
+    ($x:expr, $($ys:expr),*) => {{
+        let t = min!($($ys),*);
+        if $x < t { $x } else { t }
+    }}
+}
+#[macro_export]
+macro_rules! max {
+    (.. $x:expr) => {{
+        let mut it = $x.iter();
+        it.next().map(|z| it.fold(z, |x, y| max!(x, y)))
+    }};
+    ($x:expr) => ($x);
+    ($x:expr, $($ys:expr),*) => {{
+        let t = max!($($ys),*);
+        if $x > t { $x } else { t }
+    }}
+}
+#[macro_export]
+macro_rules! trace {
+    ($x:expr) => {
+        #[cfg(debug_assertions)]
+        eprintln!(">>> {} = {:?}", stringify!($x), $x)
+    };
+    ($($xs:expr),*) => { trace!(($($xs),*)) }
+}
+#[macro_export]
+macro_rules! put {
+    (yes) => {println!("Yes")}; (no) => {println!("No")};
+    (# $a:ident) => {println!("{}", stringify!($a))};
+    (.. $x:expr) => {{
+        let mut it = $x.iter();
+        if let Some(x) = it.next() { print!("{}", x); }
+        for x in it { print!(" {}", x); }
+        println!("");
+    }};
+    ($x:expr) => { println!("{}", $x) };
+    ($x:expr, $($xs:expr),*) => { print!("{} ", $x); put!($($xs),*) }
+}
+#[macro_export]
+macro_rules! ndarray {
+    ($x:expr;) => {
+        $x
+    };
+    ($x:expr; $size:expr $( , $rest:expr )*) => {
+        vec![ndarray!($x; $($rest),*); $size]
+    };
+}
+
+// }}}
