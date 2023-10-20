d078499837d75bb95f69ebedc870b8a89a65eacf
cympfh <cympfh@gmail.com>
2023-10-20 12:22:42 +0900

    ADT/20231017; realtime

---
+++ atcoder/adt/adt_all_20231017_1/src/bin/g.rs
@@ -0,0 +1,237 @@
+#![allow(unused_imports, unused_macros, dead_code)]
+use std::{cmp::*, collections::*};
+
+fn main() {
+    let mut sc = Scanner::default();
+    let n: usize = sc.cin();
+    let m: usize = sc.cin();
+    let ss: Vec<String> = sc.vec(n);
+    let ts: HashSet<String> = sc.vec(m).into_iter().collect();
+
+    let mut prefixes = HashSet::new();
+    for t in ts.iter() {
+        let n = t.len();
+        let tt: Vec<char> = t.chars().collect();
+        for i in 1..n {
+            if tt[i - 1] == '_' && tt[i] == '_' {
+                continue;
+            }
+            if tt[i - 1] != '_' && tt[i] != '_' {
+                continue;
+            }
+            prefixes.insert(t[..i].to_string());
+        }
+        prefixes.insert(t.clone());
+    }
+    trace!(&prefixes);
+
+    for p in Permutation::new(n) {
+        let mut c = ss[p[0]].clone();
+        for i in 1..n {
+            c.push('_');
+            while prefixes.contains(&c) {
+                c.push('_');
+                if c.len() > 17 {
+                    c.push('@');
+                    c.push('@');
+                    break;
+                }
+            }
+            c.push_str(ss[p[i]].as_str());
+        }
+        trace!(&p, &c);
+        if 3 <= c.len() && c.len() <= 16 && !ts.contains(&c) {
+            put!(c);
+            return;
+        }
+    }
+    put!(-1);
+}
+
+// @num/iter/perm
+/// Number - Iterator - Factorial Permutation (n!)
+pub struct Permutation {
+    n: usize,
+    idx: usize,
+    done: bool,
+}
+impl Permutation {
+    pub fn new(n: usize) -> Permutation {
+        Permutation {
+            n,
+            idx: 0,
+            done: false,
+        }
+    }
+    pub fn from(mut perm: Vec<usize>) -> Permutation {
+        let n = perm.len();
+        let mut idx = 0;
+        let mut fact: usize = (1..n).product();
+        for i in 0..n {
+            if i > 0 {
+                fact /= n - i;
+            }
+            idx += perm[i] * fact;
+            for j in i + 1..n {
+                if perm[j] > perm[i] {
+                    perm[j] -= 1;
+                }
+            }
+        }
+        Permutation {
+            n,
+            idx,
+            done: false,
+        }
+    }
+    pub fn to_vec(&mut self) -> Option<Vec<usize>> {
+        if self.done {
+            return None;
+        }
+        if self.n == 0 {
+            self.done = true;
+            return Some(vec![]);
+        }
+        let mut r = vec![0; self.n];
+        let mut idx = self.idx;
+        for k in 1..self.n {
+            r[k] = idx % (k + 1);
+            idx /= k + 1;
+        }
+        if idx > 0 {
+            self.done = true;
+            return None;
+        }
+        r.reverse();
+        let mut b = vec![true; self.n];
+        b[r[0]] = false;
+        for k in 1..self.n {
+            let mut count = 0;
+            for j in 0..self.n {
+                if b[j] {
+                    if count == r[k] {
+                        r[k] = j;
+                        b[j] = false;
+                        break;
+                    }
+                    count += 1;
+                }
+            }
+        }
+        Some(r)
+    }
+}
+impl Iterator for Permutation {
+    type Item = Vec<usize>;
+    fn next(&mut self) -> Option<Vec<usize>> {
+        let r = self.to_vec();
+        self.idx += 1;
+        r
+    }
+}
+
+// {{{
+use std::io::{self, Write};
+use std::str::FromStr;
+#[derive(Default)]
+pub struct Scanner {
+    buffer: VecDeque<String>,
+}
+impl Scanner {
+    pub fn cin<T: FromStr>(&mut self) -> T {
+        while self.buffer.is_empty() {
+            let mut line = String::new();
+            let _ = io::stdin().read_line(&mut line);
+            self.buffer = line.split_whitespace().map(|w| String::from(w)).collect();
+        }
+        self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap()
+    }
+    pub fn usize1(&mut self) -> usize {
+        self.cin::<usize>() - 1
+    }
+    pub fn chars(&mut self) -> Vec<char> {
+        self.cin::<String>().chars().collect()
+    }
+    pub fn vec<T: FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.cin()).collect()
+    }
+    pub fn pair<S: FromStr, T: FromStr>(&mut self) -> (S, T) {
+        (self.cin::<S>(), self.cin::<T>())
+    }
+}
+fn flush() {
+    io::stdout().flush().unwrap();
+}
+#[macro_export]
+macro_rules! min {
+    (.. $x:expr) => {{
+        let mut it = $x.iter();
+        it.next().map(|z| it.fold(z, |x, y| min!(x, y)))
+    }};
+    ($x:expr) => ($x);
+    ($x:expr, $($ys:expr),*) => {{
+        let t = min!($($ys),*);
+        if $x < t { $x } else { t }
+    }}
+}
+#[macro_export]
+macro_rules! max {
+    (.. $x:expr) => {{
+        let mut it = $x.iter();
+        it.next().map(|z| it.fold(z, |x, y| max!(x, y)))
+    }};
+    ($x:expr) => ($x);
+    ($x:expr, $($ys:expr),*) => {{
+        let t = max!($($ys),*);
+        if $x > t { $x } else { t }
+    }}
+}
+#[macro_export]
+macro_rules! trace {
+    (# $a:ident $(,)? $(;)? $($xs:expr),* $(,)? ) => {
+        #[cfg(debug_assertions)]
+        eprintln!("[{}] {} = {:?}", stringify!($a), stringify!($($xs),*), ($($xs),*))
+    };
+    ($($xs:expr),* $(,)?) => {
+        #[cfg(debug_assertions)]
+        eprintln!(">>> {} = {:?}", stringify!($($xs),*), ($($xs),*))
+    };
+}
+#[macro_export]
+macro_rules! put {
+    (# $a:ident) => {println!("{}", stringify!($a))};
+    (.. $x:expr) => {{
+        let mut it = $x.iter();
+        if let Some(x) = it.next() { print!("{}", x); }
+        for x in it { print!(" {}", x); }
+        println!("");
+    }};
+    ($x:expr) => { println!("{}", $x) };
+    ($x:expr, $($xs:expr),*) => { print!("{} ", $x); put!($($xs),*) }
+}
+#[macro_export]
+macro_rules! ndarray {
+    ($x:expr;) => { $x };
+    ($x:expr; $size:expr $( , $rest:expr )*) => {
+        vec![ndarray!($x; $($rest),*); $size]
+    };
+}
+/// Array-indexing by i64.
+/// (Vec<Vec<..<T>..>>; i64, i64, ..) => Option<T>
+#[macro_export]
+macro_rules! at {
+    ($s:expr;) => { Some($s) };
+    ($s:expr; $idx:expr $(,$args:expr)* $(,)?) => {
+        if 0 <= $idx {
+            let idx_usize = $idx as usize;
+            if idx_usize < $s.len() {
+                at!($s[idx_usize]; $($args),*)
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+}
+// }}}
