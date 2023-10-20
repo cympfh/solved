7360fdd53a068f724a405e0066afe012a8b91093
cympfh <cympfh@gmail.com>
2023-10-20 13:46:05 +0900

    adt/20231017; virtual

---
+++ atcoder/adt/adt_all_20231018_1/src/bin/g.rs
@@ -0,0 +1,166 @@
+#![allow(unused_imports, unused_macros, dead_code)]
+use std::{cmp::*, collections::*};
+
+fn main() {
+    let mut sc = Scanner::default();
+    let n: usize = sc.cin();
+    let a: Vec<i64> = sc.vec(n);
+
+    // (left, right, length)
+    let mut spans: Vec<(i64, i64, i64)> = vec![];
+    for i in 1..n {
+        if i % 2 == 0 {
+            let left = a[i - 1];
+            let right = a[i];
+            spans.push((left, right, right - left));
+        }
+    }
+    trace!(&spans);
+
+    // memo[x] = [0, x) で何分寝た?
+    let mut memo = BTreeMap::new();
+    {
+        let mut acc = 0;
+        memo.insert(0, 0);
+        for &(left, right, length) in spans.iter() {
+            memo.insert(left, acc);
+            acc += length;
+            memo.insert(right, acc);
+        }
+    }
+    trace!(&memo);
+
+    let spans: BTreeMap<i64, _> = spans
+        .into_iter()
+        .map(|(left, right, length)| (left, (left, right, length)))
+        .collect();
+    trace!(&spans);
+
+    let sumlength = |x: i64| -> i64 {
+        trace!(x, spans.range(..=x).next_back());
+        match spans.range(..=x).next_back() {
+            Some((_, &(left, right, _))) => {
+                if left <= x && x <= right {
+                    memo[&left] + x - left
+                } else {
+                    memo[&right]
+                }
+            }
+            _ => 0,
+        }
+    };
+
+    let q: usize = sc.cin();
+    for _ in 0..q {
+        let left: i64 = sc.cin();
+        let right: i64 = sc.cin();
+        let ans = sumlength(right) - sumlength(left);
+        put!(ans);
+    }
+}
+
+// {{{
+use std::io::{self, Write};
+use std::str::FromStr;
+#[derive(Default)]
+pub struct Scanner {
+    buffer: VecDeque<String>,
+}
+impl Scanner {
+    pub fn cin<T: FromStr>(&mut self) -> T {
+        while self.buffer.is_empty() {
+            let mut line = String::new();
+            let _ = io::stdin().read_line(&mut line);
+            self.buffer = line.split_whitespace().map(|w| String::from(w)).collect();
+        }
+        self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap()
+    }
+    pub fn usize1(&mut self) -> usize {
+        self.cin::<usize>() - 1
+    }
+    pub fn chars(&mut self) -> Vec<char> {
+        self.cin::<String>().chars().collect()
+    }
+    pub fn vec<T: FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.cin()).collect()
+    }
+    pub fn pair<S: FromStr, T: FromStr>(&mut self) -> (S, T) {
+        (self.cin::<S>(), self.cin::<T>())
+    }
+}
+fn flush() {
+    io::stdout().flush().unwrap();
+}
+#[macro_export]
+macro_rules! min {
+    (.. $x:expr) => {{
+        let mut it = $x.iter();
+        it.next().map(|z| it.fold(z, |x, y| min!(x, y)))
+    }};
+    ($x:expr) => ($x);
+    ($x:expr, $($ys:expr),*) => {{
+        let t = min!($($ys),*);
+        if $x < t { $x } else { t }
+    }}
+}
+#[macro_export]
+macro_rules! max {
+    (.. $x:expr) => {{
+        let mut it = $x.iter();
+        it.next().map(|z| it.fold(z, |x, y| max!(x, y)))
+    }};
+    ($x:expr) => ($x);
+    ($x:expr, $($ys:expr),*) => {{
+        let t = max!($($ys),*);
+        if $x > t { $x } else { t }
+    }}
+}
+#[macro_export]
+macro_rules! trace {
+    (# $a:ident $(,)? $(;)? $($xs:expr),* $(,)? ) => {
+        #[cfg(debug_assertions)]
+        eprintln!("[{}] {} = {:?}", stringify!($a), stringify!($($xs),*), ($($xs),*))
+    };
+    ($($xs:expr),* $(,)?) => {
+        #[cfg(debug_assertions)]
+        eprintln!(">>> {} = {:?}", stringify!($($xs),*), ($($xs),*))
+    };
+}
+#[macro_export]
+macro_rules! put {
+    (# $a:ident) => {println!("{}", stringify!($a))};
+    (.. $x:expr) => {{
+        let mut it = $x.iter();
+        if let Some(x) = it.next() { print!("{}", x); }
+        for x in it { print!(" {}", x); }
+        println!("");
+    }};
+    ($x:expr) => { println!("{}", $x) };
+    ($x:expr, $($xs:expr),*) => { print!("{} ", $x); put!($($xs),*) }
+}
+#[macro_export]
+macro_rules! ndarray {
+    ($x:expr;) => { $x };
+    ($x:expr; $size:expr $( , $rest:expr )*) => {
+        vec![ndarray!($x; $($rest),*); $size]
+    };
+}
+/// Array-indexing by i64.
+/// (Vec<Vec<..<T>..>>; i64, i64, ..) => Option<T>
+#[macro_export]
+macro_rules! at {
+    ($s:expr;) => { Some($s) };
+    ($s:expr; $idx:expr $(,$args:expr)* $(,)?) => {
+        if 0 <= $idx {
+            let idx_usize = $idx as usize;
+            if idx_usize < $s.len() {
+                at!($s[idx_usize]; $($args),*)
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+}
+// }}}
