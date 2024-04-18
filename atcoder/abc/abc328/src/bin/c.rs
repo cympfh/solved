ff1ffb75d993ce179bcf4f7ef1639e982765934e
cympfh <cympfh@gmail.com>
2024-04-18 19:26:46 +0900

    ABC328; virtual

---
+++ atcoder/abc/abc328/src/bin/c.rs
@@ -0,0 +1,239 @@
+#![allow(unused_imports, unused_macros, dead_code)]
+use std::{cmp::*, collections::*};
+
+fn main() {
+    let mut sc = Scanner::default();
+    let n: usize = sc.cin();
+    let q: usize = sc.cin();
+    let s: Vec<char> = sc.chars();
+    let mut b: Vec<i64> = vec![0; n];
+    for i in 0..n - 1 {
+        if s[i] == s[i + 1] {
+            b[i] = 1;
+        }
+    }
+    let bsum = Cumsum1d::new(&b);
+    for _ in 0..q {
+        let left = sc.usize1();
+        let right = sc.usize1();
+        put!(bsum.sum(left..right));
+    }
+}
+
+// @sequence/cumsum1d
+// @algebra/group_additive
+/// Algebra - AGroup (Additive Group) (+, -, 0)
+pub trait AGroup:
+    std::ops::Add<Output = Self>
+    + std::ops::Sub<Output = Self>
+    + std::ops::Neg<Output = Self>
+    + std::iter::Sum
+where
+    Self: std::marker::Sized,
+{
+    fn zero() -> Self;
+}
+
+#[macro_export]
+macro_rules! agroup {
+    (
+        $type:ty where [ $( $params:tt )* ] ;
+        zero = $zero:expr ;
+        add($self:ident, $y:ident) = $code:block ;
+        neg($self_neg:ident) = $code_neg:block
+        $(;)*
+    ) => {
+        impl<$($params)*> std::ops::Add for $type {
+            type Output = Self;
+            fn add($self, $y: Self) -> Self { $code }
+        }
+        impl<$($params)*> std::ops::Neg for $type {
+            type Output = Self;
+            fn neg($self_neg) -> Self { $code_neg }
+        }
+        impl<$($params)*> std::ops::Sub for $type {
+            type Output = Self;
+            fn sub($self, other: Self) -> Self { ($self) + (-other) }
+        }
+        impl<$($params)*> std::ops::AddAssign for $type where Self: Clone {
+            fn add_assign(&mut $self, $y: Self) {
+                *$self = (*$self).clone() + $y;
+            }
+        }
+        impl<$($params)*> std::ops::SubAssign for $type where Self: Clone {
+            fn sub_assign(&mut $self, $y: Self) {
+                *$self = (*$self).clone() - $y;
+            }
+        }
+        impl<$($params)*> std::iter::Sum for $type {
+            fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
+                iter.fold(Self::zero(), std::ops::Add::add)
+            }
+        }
+        impl<$($params)*> AGroup for $type {
+            fn zero() -> Self { $zero }
+        }
+    };
+    (
+        $type:ty ;
+        zero = $zero:expr ;
+        add($self:ident, $y:ident) = $code:block ;
+        neg($self_neg:ident) = $code_neg:block
+        $(;)*
+    ) => {
+        agroup! { $type where []; zero = $zero; add($self, $y) = $code; neg($self_neg) = $code_neg; }
+    };
+}
+
+impl AGroup for i64 {
+    fn zero() -> Self {
+        0
+    }
+}
+impl AGroup for i128 {
+    fn zero() -> Self {
+        0
+    }
+}
+impl AGroup for f64 {
+    fn zero() -> Self {
+        0.0
+    }
+}
+
+/// Sequence - Cumulative Summation 1D of Additive Group (+, 0)
+#[derive(Debug)]
+pub struct Cumsum1d<T>(Vec<T>);
+impl<T: Copy + AGroup> Cumsum1d<T> {
+    pub fn new(xs: &Vec<T>) -> Self {
+        let mut ac = T::zero();
+        let mut arr = vec![T::zero(); xs.len()];
+        for i in 0..arr.len() {
+            ac = ac + xs[i];
+            arr[i] = ac;
+        }
+        Self(arr)
+    }
+    /// sum of [0, idx)
+    fn sum_up(&self, idx: usize) -> T {
+        if idx > 0 {
+            self.0[idx - 1]
+        } else {
+            T::zero()
+        }
+    }
+    /// sum(i..j) = sum of [i, j)
+    pub fn sum(&self, range: std::ops::Range<usize>) -> T {
+        if range.start >= range.end {
+            T::zero()
+        } else {
+            self.sum_up(range.end) - self.sum_up(range.start)
+        }
+    }
+}
+
+// {{{
+use std::io::{self, Write};
+use std::str::FromStr;
+#[derive(Default)]
+pub struct Scanner {
+    buffer: VecDeque<String>,
+}
+impl Scanner {
+    pub fn cin<T: FromStr>(&mut self) -> T {
+        while self.buffer.is_empty() {
+            let mut line = String::new();
+            let _ = io::stdin().read_line(&mut line);
+            self.buffer = line.split_whitespace().map(|w| String::from(w)).collect();
+        }
+        self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap()
+    }
+    pub fn usize1(&mut self) -> usize {
+        self.cin::<usize>() - 1
+    }
+    pub fn chars(&mut self) -> Vec<char> {
+        self.cin::<String>().chars().collect()
+    }
+    pub fn vec<T: FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.cin()).collect()
+    }
+    pub fn pair<S: FromStr, T: FromStr>(&mut self) -> (S, T) {
+        (self.cin::<S>(), self.cin::<T>())
+    }
+}
+fn flush() {
+    io::stdout().flush().unwrap();
+}
+#[macro_export]
+macro_rules! min {
+    (.. $x:expr) => {{
+        let mut it = $x.iter();
+        it.next().map(|z| it.fold(z, |x, y| min!(x, y)))
+    }};
+    ($x:expr) => ($x);
+    ($x:expr, $($ys:expr),* $(,)*) => {{
+        let t = min!($($ys),*);
+        if $x < t { $x } else { t }
+    }}
+}
+#[macro_export]
+macro_rules! max {
+    (.. $x:expr) => {{
+        let mut it = $x.iter();
+        it.next().map(|z| it.fold(z, |x, y| max!(x, y)))
+    }};
+    ($x:expr) => ($x);
+    ($x:expr, $($ys:expr),* $(,)*) => {{
+        let t = max!($($ys),*);
+        if $x > t { $x } else { t }
+    }}
+}
+#[macro_export]
+macro_rules! trace {
+    (# $a:ident $(,)? $(;)? $($xs:expr),* $(,)? ) => {
+        #[cfg(debug_assertions)]
+        eprintln!("[{}] {} = {:?}", stringify!($a), stringify!($($xs),*), ($($xs),*))
+    };
+    ($($xs:expr),* $(,)?) => {
+        #[cfg(debug_assertions)]
+        eprintln!(">>> {} = {:?}", stringify!($($xs),*), ($($xs),*))
+    };
+}
+#[macro_export]
+macro_rules! put {
+    (# $a:ident) => {println!("{}", stringify!($a))};
+    (.. $x:expr) => {{
+        let mut it = $x.iter();
+        if let Some(x) = it.next() { print!("{}", x); }
+        for x in it { print!(" {}", x); }
+        println!("");
+    }};
+    ($x:expr) => { println!("{}", $x) };
+    ($x:expr, $($xs:expr),*) => { print!("{} ", $x); put!($($xs),*) }
+}
+#[macro_export]
+macro_rules! ndarray {
+    ($x:expr;) => { $x };
+    ($x:expr; $size:expr $( , $rest:expr )*) => {
+        vec![ndarray!($x; $($rest),*); $size]
+    };
+}
+/// Array-indexing by i64.
+/// (Vec<Vec<..<T>..>>; i64, i64, ..) => Option<T>
+#[macro_export]
+macro_rules! at {
+    ($s:expr;) => { Some($s) };
+    ($s:expr; $idx:expr $(,$args:expr)* $(,)?) => {
+        if 0 <= $idx {
+            let idx_usize = $idx as usize;
+            if idx_usize < $s.len() {
+                at!($s[idx_usize]; $($args),*)
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+}
+// }}}
---
+++ atcoder/abc/abc328/src/bin/e.rs
@@ -0,0 +1,489 @@
+#![allow(unused_imports, unused_macros, dead_code)]
+use std::{cmp::*, collections::*};
+
+fn main() {
+    let mut sc = Scanner::default();
+    let n: usize = sc.cin();
+    let m: usize = sc.cin();
+    let k: u64 = sc.cin();
+    let edges: Vec<(usize, usize, u64)> = (0..m)
+        .map(|_| {
+            let u = sc.usize1();
+            let v = sc.usize1();
+            let w: u64 = sc.cin();
+            (u, v, w)
+        })
+        .collect();
+    let mut cost = ndarray![989898127; n, n];
+    for &(u, v, w) in edges.iter() {
+        cost[u][v] = w;
+        cost[v][u] = w;
+    }
+    use Hyper::*;
+    let mut ans = Inf;
+    for p in PowerPermutation::new(n - 1, n - 1) {
+        let mut uf = UnionFind::new(n);
+        let mut ok = true;
+        let mut sum = 0;
+        for i in 0..n - 1 {
+            let j = (i + 1 + p[i]) % n;
+            if uf.is_same(i, j) {
+                ok = false;
+            }
+            if cost[i][j] == 989898127 {
+                ok = false;
+            }
+            uf.merge(i, j);
+            sum = (sum + cost[i][j]) % k;
+        }
+        if ok {
+            ans = min!(ans, Real(sum));
+        }
+    }
+    put!(ans.unwrap());
+}
+
+// @algebra/hyper
+// @algebra/group_additive
+/// Algebra - AGroup (Additive Group) (+, -, 0)
+pub trait AGroup:
+    std::ops::Add<Output = Self>
+    + std::ops::Sub<Output = Self>
+    + std::ops::Neg<Output = Self>
+    + std::iter::Sum
+where
+    Self: std::marker::Sized,
+{
+    fn zero() -> Self;
+}
+
+#[macro_export]
+macro_rules! agroup {
+    (
+        $type:ty where [ $( $params:tt )* ] ;
+        zero = $zero:expr ;
+        add($self:ident, $y:ident) = $code:block ;
+        neg($self_neg:ident) = $code_neg:block
+        $(;)*
+    ) => {
+        impl<$($params)*> std::ops::Add for $type {
+            type Output = Self;
+            fn add($self, $y: Self) -> Self { $code }
+        }
+        impl<$($params)*> std::ops::Neg for $type {
+            type Output = Self;
+            fn neg($self_neg) -> Self { $code_neg }
+        }
+        impl<$($params)*> std::ops::Sub for $type {
+            type Output = Self;
+            fn sub($self, other: Self) -> Self { ($self) + (-other) }
+        }
+        impl<$($params)*> std::ops::AddAssign for $type where Self: Clone {
+            fn add_assign(&mut $self, $y: Self) {
+                *$self = (*$self).clone() + $y;
+            }
+        }
+        impl<$($params)*> std::ops::SubAssign for $type where Self: Clone {
+            fn sub_assign(&mut $self, $y: Self) {
+                *$self = (*$self).clone() - $y;
+            }
+        }
+        impl<$($params)*> std::iter::Sum for $type {
+            fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
+                iter.fold(Self::zero(), std::ops::Add::add)
+            }
+        }
+        impl<$($params)*> AGroup for $type {
+            fn zero() -> Self { $zero }
+        }
+    };
+    (
+        $type:ty ;
+        zero = $zero:expr ;
+        add($self:ident, $y:ident) = $code:block ;
+        neg($self_neg:ident) = $code_neg:block
+        $(;)*
+    ) => {
+        agroup! { $type where []; zero = $zero; add($self, $y) = $code; neg($self_neg) = $code_neg; }
+    };
+}
+
+impl AGroup for i64 {
+    fn zero() -> Self {
+        0
+    }
+}
+impl AGroup for i128 {
+    fn zero() -> Self {
+        0
+    }
+}
+impl AGroup for f64 {
+    fn zero() -> Self {
+        0.0
+    }
+}
+
+// @algebra/monoid
+/// Algebra - Def of Monoid (*, 1)
+pub trait Monoid: std::ops::Mul<Output = Self> + std::iter::Product
+where
+    Self: std::marker::Sized,
+{
+    fn one() -> Self;
+}
+
+#[macro_export]
+macro_rules! monoid {
+    (
+        $type:ty where [ $( $params:tt )* ];
+        one = $one:expr;
+        mul($self:ident, $y:ident) = $code:block
+        $(;)*
+    ) => {
+        impl<$($params)*> std::ops::Mul for $type {
+            type Output = Self;
+            fn mul($self, $y: Self) -> Self { $code }
+        }
+        impl<$($params)*> std::ops::MulAssign for $type where Self: Clone {
+            fn mul_assign(&mut $self, $y: Self) {
+                *$self = (*$self).clone() * $y;
+            }
+        }
+        impl<$($params)*> std::iter::Product for $type {
+            fn product<I: Iterator<Item = Self>>(iter: I) -> Self {
+                iter.fold(Self::one(), std::ops::Mul::mul)
+            }
+        }
+        impl<$($params)*> Monoid for $type {
+            fn one() -> Self { $one }
+        }
+    };
+    (
+        $type:ty;
+        one = $one:expr;
+        mul($self:ident, $y:ident) = $code:block
+        $(;)*
+    ) => {
+        monoid! { $type where []; one = $one; mul($self, $y) = $code; }
+    };
+}
+
+impl Monoid for i64 {
+    fn one() -> Self {
+        1
+    }
+}
+impl Monoid for i128 {
+    fn one() -> Self {
+        1
+    }
+}
+impl Monoid for f64 {
+    fn one() -> Self {
+        1.0
+    }
+}
+
+// @algebra/ring
+/// Algebra - Ring ((+, 0), (*, 1))
+pub trait Ring: AGroup + Monoid {}
+
+#[macro_export]
+macro_rules! ring {
+    (
+        $type:ty where [ $( $params:tt )* ];
+        div($self:ident, $other:ident) = $code:block
+        $(;)*
+    ) => {
+        impl<$($params)*> std::ops::Div for $type {
+            type Output = Self;
+            fn div($self, $other: Self) -> Self { $code }
+        }
+        impl<$($params)*> std::ops::DivAssign for $type where Self: Clone {
+            fn div_assign(&mut $self, $other: Self) { *$self = (*$self).clone() / $other; }
+        }
+        impl Ring for $type {}
+    };
+    (
+        $type:ty;
+        div($self:ident, $other:ident) = $code:block
+        $(;)*
+    ) => {
+        ring! { $type where []; div($self, $other) = $code; }
+    };
+}
+
+impl Ring for i64 {}
+impl Ring for f64 {}
+
+/// Algebra - Hyper Numbers (numbers with infinity)
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Hyper<X> {
+    NegInf,
+    Real(X),
+    Inf,
+}
+use Hyper::{Inf, NegInf, Real};
+impl<X> Hyper<X> {
+    pub fn unwrap(self) -> X {
+        if let Hyper::Real(x) = self {
+            x
+        } else {
+            panic!("Could not unwrap Hyper")
+        }
+    }
+}
+agroup! {
+    Hyper<X> where [X: AGroup];
+    zero = Real(X::zero());
+    add(self, other) = {
+        match (self, other) {
+            (Real(x), Real(y)) => Real(x + y),
+            (Inf, _) => Inf,
+            (_, Inf) => Inf,
+            _ => NegInf,
+        }
+    };
+    neg(self) = {
+        match self {
+            Inf => NegInf,
+            NegInf => Inf,
+            Real(x) => Real(-x),
+        }
+    };
+}
+monoid! {
+    Hyper<X> where [X: Monoid];
+    one = Real(X::one());
+    mul(self, other) = {
+        match (self, other) {
+            (Real(x), Real(y)) => Real(x * y),
+            (Inf, Inf) | (NegInf, NegInf) => Inf,
+            _ => NegInf,
+        }
+    };
+}
+impl<X: AGroup + Monoid> Ring for Hyper<X> {}
+impl<X: std::ops::Add<Output = X>> std::ops::Add<X> for Hyper<X> {
+    type Output = Self;
+    fn add(self, y: X) -> Hyper<X> {
+        match (self, y) {
+            (Real(x), y) => Real(x + y),
+            (Inf, _) => Inf,
+            _ => NegInf,
+        }
+    }
+}
+impl<X: Clone + AGroup> std::ops::AddAssign<X> for Hyper<X> {
+    fn add_assign(&mut self, y: X) {
+        *self = (*self).clone() + Real(y);
+    }
+}
+
+// @set/union_find
+/// Set - Union-Find
+#[derive(Debug, Clone)]
+pub struct UnionFind {
+    data: Vec<UF>,
+}
+
+#[derive(Debug, Clone)]
+enum UF {
+    Root(usize),
+    Child(usize),
+}
+
+impl UnionFind {
+    pub fn new(n: usize) -> Self {
+        UnionFind {
+            data: vec![UF::Root(1); n],
+        }
+    }
+    pub fn root(&mut self, x: usize) -> usize {
+        match self.data[x] {
+            UF::Root(_) => x,
+            UF::Child(parent) => {
+                let root = self.root(parent);
+                self.data[x] = UF::Child(root);
+                root
+            }
+        }
+    }
+    pub fn is_same(&mut self, x: usize, y: usize) -> bool {
+        self.root(x) == self.root(y)
+    }
+    pub fn size(&mut self, x: usize) -> usize {
+        let r = self.root(x);
+        match self.data[r] {
+            UF::Root(size) => size,
+            UF::Child(_) => 0,
+        }
+    }
+    pub fn merge(&mut self, x: usize, y: usize) {
+        let root_x = self.root(x);
+        let root_y = self.root(y);
+        if root_x != root_y {
+            let size_x = self.size(root_x);
+            let size_y = self.size(root_y);
+            let (i, j) = if size_x > size_y {
+                (root_x, root_y)
+            } else {
+                (root_y, root_x)
+            };
+            self.data[i] = UF::Root(size_x + size_y);
+            self.data[j] = UF::Child(i);
+        }
+    }
+}
+
+// @num/iter/power
+/// Number - Iterator - Power Permutation (pow(n, m))
+pub struct PowerPermutation {
+    n: usize,
+    m: usize,
+    ar: Vec<usize>,
+    done: bool,
+}
+impl PowerPermutation {
+    pub fn new(n: usize, m: usize) -> Self {
+        Self {
+            n,
+            m,
+            ar: vec![0; m],
+            done: false,
+        }
+    }
+}
+impl Iterator for PowerPermutation {
+    type Item = Vec<usize>;
+    fn next(&mut self) -> Option<Vec<usize>> {
+        if self.done {
+            return None;
+        }
+        if self.m == 0 {
+            self.done = true;
+            return Some(self.ar.clone());
+        }
+        if self.ar[self.m - 1] >= self.n {
+            return None;
+        }
+        let r = self.ar.clone();
+        self.ar[0] += 1;
+        for i in 0..self.m - 1 {
+            if self.ar[i] == self.n {
+                self.ar[i] = 0;
+                self.ar[i + 1] += 1;
+            } else {
+                break;
+            }
+        }
+        Some(r)
+    }
+}
+
+// {{{
+use std::io::{self, Write};
+use std::str::FromStr;
+#[derive(Default)]
+pub struct Scanner {
+    buffer: VecDeque<String>,
+}
+impl Scanner {
+    pub fn cin<T: FromStr>(&mut self) -> T {
+        while self.buffer.is_empty() {
+            let mut line = String::new();
+            let _ = io::stdin().read_line(&mut line);
+            self.buffer = line.split_whitespace().map(|w| String::from(w)).collect();
+        }
+        self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap()
+    }
+    pub fn usize1(&mut self) -> usize {
+        self.cin::<usize>() - 1
+    }
+    pub fn chars(&mut self) -> Vec<char> {
+        self.cin::<String>().chars().collect()
+    }
+    pub fn vec<T: FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.cin()).collect()
+    }
+    pub fn pair<S: FromStr, T: FromStr>(&mut self) -> (S, T) {
+        (self.cin::<S>(), self.cin::<T>())
+    }
+}
+fn flush() {
+    io::stdout().flush().unwrap();
+}
+#[macro_export]
+macro_rules! min {
+    (.. $x:expr) => {{
+        let mut it = $x.iter();
+        it.next().map(|z| it.fold(z, |x, y| min!(x, y)))
+    }};
+    ($x:expr) => ($x);
+    ($x:expr, $($ys:expr),* $(,)*) => {{
+        let t = min!($($ys),*);
+        if $x < t { $x } else { t }
+    }}
+}
+#[macro_export]
+macro_rules! max {
+    (.. $x:expr) => {{
+        let mut it = $x.iter();
+        it.next().map(|z| it.fold(z, |x, y| max!(x, y)))
+    }};
+    ($x:expr) => ($x);
+    ($x:expr, $($ys:expr),* $(,)*) => {{
+        let t = max!($($ys),*);
+        if $x > t { $x } else { t }
+    }}
+}
+#[macro_export]
+macro_rules! trace {
+    (# $a:ident $(,)? $(;)? $($xs:expr),* $(,)? ) => {
+        #[cfg(debug_assertions)]
+        eprintln!("[{}] {} = {:?}", stringify!($a), stringify!($($xs),*), ($($xs),*))
+    };
+    ($($xs:expr),* $(,)?) => {
+        #[cfg(debug_assertions)]
+        eprintln!(">>> {} = {:?}", stringify!($($xs),*), ($($xs),*))
+    };
+}
+#[macro_export]
+macro_rules! put {
+    (# $a:ident) => {println!("{}", stringify!($a))};
+    (.. $x:expr) => {{
+        let mut it = $x.iter();
+        if let Some(x) = it.next() { print!("{}", x); }
+        for x in it { print!(" {}", x); }
+        println!("");
+    }};
+    ($x:expr) => { println!("{}", $x) };
+    ($x:expr, $($xs:expr),*) => { print!("{} ", $x); put!($($xs),*) }
+}
+#[macro_export]
+macro_rules! ndarray {
+    ($x:expr;) => { $x };
+    ($x:expr; $size:expr $( , $rest:expr )*) => {
+        vec![ndarray!($x; $($rest),*); $size]
+    };
+}
+/// Array-indexing by i64.
+/// (Vec<Vec<..<T>..>>; i64, i64, ..) => Option<T>
+#[macro_export]
+macro_rules! at {
+    ($s:expr;) => { Some($s) };
+    ($s:expr; $idx:expr $(,$args:expr)* $(,)?) => {
+        if 0 <= $idx {
+            let idx_usize = $idx as usize;
+            if idx_usize < $s.len() {
+                at!($s[idx_usize]; $($args),*)
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+}
+// }}}
