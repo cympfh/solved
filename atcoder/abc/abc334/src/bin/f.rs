71b2966dd02a6e22beab86fc65023bf86aff8354
cympfh <cympfh@gmail.com>
2024-04-26 17:08:57 +0900

    ABC334; virtual; Fは時間超えた

---
+++ atcoder/abc/abc334/src/bin/f.rs
@@ -0,0 +1,560 @@
+#![allow(unused_imports, unused_macros, dead_code)]
+use std::{cmp::*, collections::*};
+
+type P = (f64, f64);
+
+fn diff(x: P, y: P) -> P {
+    (x.0 - y.0, x.1 - y.1)
+}
+fn norm(x: P) -> f64 {
+    (x.0.powi(2) + x.1.powi(2)).sqrt()
+}
+
+fn main() {
+    let mut sc = Scanner::default();
+    let n: usize = sc.cin();
+    let k: usize = sc.cin();
+    let s: (f64, f64) = (sc.cin(), sc.cin());
+    let ps: Vec<(f64, f64)> = (0..n).map(|_| (sc.cin(), sc.cin())).collect();
+
+    let ps: Vec<P> = ps.into_iter().map(|(x, y)| (x - s.0, y - s.1)).collect();
+
+    // dp[i][k] = 子供 i をちょうど訪れ終わった and (残りのプレゼント + 訪れた頂点数) が k,
+    // という状態での最小距離
+    let mut dp: LazySegmentTree<MinInt<Float>, M> = LazySegmentTree::new(n + k + 10);
+    dp.update(k..k + 1, M::Update(Float(0.0)));
+
+    for i in 1..n {
+        trace!(dp.product(i..k + i));
+        let min = dp.product(i..k + i).unwrap();
+        dp.update(i..k + i, M::Add(Float(norm(diff(ps[i], ps[i - 1])))));
+        dp.update(
+            k + i..k + i + 1,
+            M::Update(Float(min.0 + norm(ps[i - 1]) + norm(ps[i]))),
+        );
+        // trace!(dp.to_vec());
+    }
+    trace!(dp.product(n..k + n + 1));
+    put!(dp.product(n..k + n + 1).unwrap().0 + norm(ps[0]) + norm(ps[n - 1]));
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum M {
+    Add(Float),
+    Update(Float),
+}
+monoid! {
+    M;
+    one = M::Add(Float(0.0));
+    mul(self, m) = {
+        // self のあとに m だと思う
+        match (self, m) {
+            (M::Add(x), M::Add(y)) => M::Add(x + y),
+            (M::Update(x), M::Add(y)) => M::Update(x + y),
+            (_, M::Update(y)) => M::Update(y),
+        }
+        // 逆か?
+        // match (self, m) {
+        //     (M::Add(x), M::Add(y)) => M::Add(x + y),
+        //     (M::Add(x), M::Update(y)) => M::Update(x + y),
+        //     (M::Update(x), _) => M::Update(x),
+        // }
+    }
+}
+impl Act<MinInt<Float>> for M {
+    fn act(&self, other: MinInt<Float>) -> MinInt<Float> {
+        match self {
+            &M::Add(x) => match other {
+                MinInt::Maximal => MinInt::Maximal,
+                MinInt::Val(y) => MinInt::Val(x + y),
+            },
+            &M::Update(x) => MinInt::Val(x),
+        }
+    }
+}
+
+// @num/float
+/// Def of Float Numbers
+#[derive(Debug, Clone, Copy)]
+pub struct Float(pub f64);
+impl Float {
+    pub fn abs(&self) -> Self {
+        Float(self.0.abs())
+    }
+    pub fn sin(&self) -> Self {
+        Float(self.0.sin())
+    }
+    pub fn cos(&self) -> Self {
+        Float(self.0.cos())
+    }
+    pub fn tan(&self) -> Self {
+        Float(self.0.tan())
+    }
+}
+agroup! {
+    Float;
+    zero = Float(0.0);
+    add(self, other) = { Float(self.0 + other.0) };
+    neg(self) = { Float(-self.0) };
+}
+monoid! {
+    Float;
+    one = Float(1.0);
+    mul(self, other) = { Float(self.0 * other.0) };
+}
+ring! {
+    Float;
+    div(self, other) = { Float(self.0 / other.0) };
+}
+impl std::ops::Rem for Float {
+    type Output = Self;
+    fn rem(self, modulo: Self) -> Float {
+        Float(self.0 % modulo.0)
+    }
+}
+impl std::ops::RemAssign for Float {
+    fn rem_assign(&mut self, modulo: Self) {
+        self.0 %= modulo.0;
+    }
+}
+impl std::cmp::PartialEq for Float {
+    fn eq(&self, other: &Self) -> bool {
+        let allow = 1e-6;
+        (self.0 - other.0).abs() < allow
+            || (self.0 - other.0).abs() / (self.0 + other.0) * 2.0 < allow
+    }
+}
+impl std::cmp::Eq for Float {}
+impl std::cmp::PartialOrd for Float {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.0.partial_cmp(&other.0)
+    }
+}
+impl std::cmp::Ord for Float {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.0.partial_cmp(&other.0).unwrap()
+    }
+}
+
+// @sequence/tree/lazy_segment_tree
+// @algebra/act
+/// Algebra - Act
+pub trait Act<X> {
+    fn act(&self, x: X) -> X;
+}
+
+/// Sequence - Lazy Segment Tree
+#[derive(Debug, Clone)]
+pub struct LazySegmentTree<X, M> {
+    length: usize,       // of leaves
+    length_upper: usize, // power of 2
+    size: usize,         // of nodes
+    data: Vec<X>,
+    act: Vec<M>,
+}
+impl<X: Copy + Monoid, M: Copy + Monoid + Act<X>> LazySegmentTree<X, M> {
+    pub fn new(length: usize) -> Self {
+        let mut length_upper = 1;
+        while length_upper < length {
+            length_upper *= 2;
+        }
+        let size = length_upper * 2 - 1;
+        let data = vec![X::one(); size];
+        let act = vec![M::one(); size];
+        LazySegmentTree {
+            length,
+            length_upper,
+            size,
+            data,
+            act,
+        }
+    }
+    pub fn from(xs: Vec<X>) -> Self {
+        let mut tree = Self::new(xs.len());
+        for i in 0..xs.len() {
+            tree.data[tree.size / 2 + i] = xs[i];
+        }
+        for i in (0..tree.size / 2).rev() {
+            tree.data[i] = tree.data[2 * i + 1] * tree.data[2 * i + 2];
+        }
+        tree
+    }
+    fn propagation(&mut self, idx: usize) {
+        if idx < self.size / 2 {
+            self.act[idx * 2 + 1] = self.act[idx * 2 + 1] * self.act[idx];
+            self.act[idx * 2 + 2] = self.act[idx * 2 + 2] * self.act[idx];
+        }
+        self.data[idx] = self.act[idx].act(self.data[idx]);
+        self.act[idx] = M::one();
+    }
+    fn update_sub(
+        &mut self,
+        range: std::ops::Range<usize>,
+        m: M,
+        idx: usize,
+        focus: std::ops::Range<usize>,
+    ) {
+        self.propagation(idx);
+        if focus.end <= range.start || range.end <= focus.start {
+            return;
+        }
+        if range.start <= focus.start && focus.end <= range.end {
+            self.act[idx] = self.act[idx] * m;
+            self.propagation(idx);
+        } else if idx < self.data.len() / 2 {
+            let mid = (focus.start + focus.end) / 2;
+            self.update_sub(range.clone(), m, idx * 2 + 1, focus.start..mid);
+            self.update_sub(range.clone(), m, idx * 2 + 2, mid..focus.end);
+            self.data[idx] = self.data[idx * 2 + 1] * self.data[idx * 2 + 2];
+        }
+    }
+    pub fn update(&mut self, range: std::ops::Range<usize>, m: M) {
+        self.update_sub(range, m, 0, 0..self.length_upper);
+    }
+    fn product_sub(
+        &mut self,
+        range: std::ops::Range<usize>,
+        idx: usize,
+        focus: std::ops::Range<usize>,
+    ) -> X {
+        self.propagation(idx);
+        if focus.end <= range.start || range.end <= focus.start {
+            X::one()
+        } else if range.start <= focus.start && focus.end <= range.end {
+            self.data[idx]
+        } else {
+            let mid = (focus.start + focus.end) / 2;
+            let a = self.product_sub(range.clone(), idx * 2 + 1, focus.start..mid);
+            let b = self.product_sub(range.clone(), idx * 2 + 2, mid..focus.end);
+            a * b
+        }
+    }
+    pub fn product(&mut self, range: std::ops::Range<usize>) -> X {
+        self.product_sub(range, 0, 0..self.length_upper)
+    }
+    pub fn index(&mut self, i: usize) -> X {
+        self.product(i..i + 1)
+    }
+    pub fn to_vec(&mut self) -> Vec<X> {
+        (0..self.length).map(|i| self.index(i)).collect()
+    }
+}
+impl<X: std::fmt::Debug, M: std::fmt::Debug> LazySegmentTree<X, M> {
+    pub fn debug(&self) {
+        #[cfg(debug_assertions)]
+        for i in 0..self.size {
+            if i > 0 && (i + 1).count_ones() == 1 {
+                eprintln!();
+            }
+            eprint!("{:?} / {:?}; ", &self.data[i], &self.act[i]);
+        }
+        eprintln!();
+    }
+}
+
+// @algebra/monoid_max
+/// Algebra - Monoid - MinInt
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum MinInt<X> {
+    Val(X),
+    Maximal,
+}
+impl<X> MinInt<X> {
+    pub fn unwrap(self) -> X {
+        match self {
+            Self::Val(x) => x,
+            _ => panic!(),
+        }
+    }
+}
+monoid! {
+    MinInt<X> where [X:Ord];
+    one = MinInt::Maximal;
+    mul(self, other) = {
+        if self < other {
+            self
+        } else {
+            other
+        }
+    }
+}
+
+// @algebra/hyper
+// @algebra/group_additive
+/// Algebra - AGroup (Additive Group) (+, -, 0)
+pub trait AGroup:
+    std::ops::Add<Output = Self>
+    + std::ops::Sub<Output = Self>
+    + std::ops::Neg<Output = Self>
+    + std::iter::Sum
+where
+    Self: std::marker::Sized,
+{
+    fn zero() -> Self;
+}
+
+#[macro_export]
+macro_rules! agroup {
+    (
+        $type:ty where [ $( $params:tt )* ] ;
+        zero = $zero:expr ;
+        add($self:ident, $y:ident) = $code:block ;
+        neg($self_neg:ident) = $code_neg:block
+        $(;)*
+    ) => {
+        impl<$($params)*> std::ops::Add for $type {
+            type Output = Self;
+            fn add($self, $y: Self) -> Self { $code }
+        }
+        impl<$($params)*> std::ops::Neg for $type {
+            type Output = Self;
+            fn neg($self_neg) -> Self { $code_neg }
+        }
+        impl<$($params)*> std::ops::Sub for $type {
+            type Output = Self;
+            fn sub($self, other: Self) -> Self { ($self) + (-other) }
+        }
+        impl<$($params)*> std::ops::AddAssign for $type where Self: Clone {
+            fn add_assign(&mut $self, $y: Self) {
+                *$self = (*$self).clone() + $y;
+            }
+        }
+        impl<$($params)*> std::ops::SubAssign for $type where Self: Clone {
+            fn sub_assign(&mut $self, $y: Self) {
+                *$self = (*$self).clone() - $y;
+            }
+        }
+        impl<$($params)*> std::iter::Sum for $type {
+            fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
+                iter.fold(Self::zero(), std::ops::Add::add)
+            }
+        }
+        impl<$($params)*> AGroup for $type {
+            fn zero() -> Self { $zero }
+        }
+    };
+    (
+        $type:ty ;
+        zero = $zero:expr ;
+        add($self:ident, $y:ident) = $code:block ;
+        neg($self_neg:ident) = $code_neg:block
+        $(;)*
+    ) => {
+        agroup! { $type where []; zero = $zero; add($self, $y) = $code; neg($self_neg) = $code_neg; }
+    };
+}
+
+impl AGroup for i64 {
+    fn zero() -> Self {
+        0
+    }
+}
+impl AGroup for i128 {
+    fn zero() -> Self {
+        0
+    }
+}
+impl AGroup for f64 {
+    fn zero() -> Self {
+        0.0
+    }
+}
+
+// @algebra/monoid
+/// Algebra - Def of Monoid (*, 1)
+pub trait Monoid: std::ops::Mul<Output = Self> + std::iter::Product
+where
+    Self: std::marker::Sized,
+{
+    fn one() -> Self;
+}
+
+#[macro_export]
+macro_rules! monoid {
+    (
+        $type:ty where [ $( $params:tt )* ];
+        one = $one:expr;
+        mul($self:ident, $y:ident) = $code:block
+        $(;)*
+    ) => {
+        impl<$($params)*> std::ops::Mul for $type {
+            type Output = Self;
+            fn mul($self, $y: Self) -> Self { $code }
+        }
+        impl<$($params)*> std::ops::MulAssign for $type where Self: Clone {
+            fn mul_assign(&mut $self, $y: Self) {
+                *$self = (*$self).clone() * $y;
+            }
+        }
+        impl<$($params)*> std::iter::Product for $type {
+            fn product<I: Iterator<Item = Self>>(iter: I) -> Self {
+                iter.fold(Self::one(), std::ops::Mul::mul)
+            }
+        }
+        impl<$($params)*> Monoid for $type {
+            fn one() -> Self { $one }
+        }
+    };
+    (
+        $type:ty;
+        one = $one:expr;
+        mul($self:ident, $y:ident) = $code:block
+        $(;)*
+    ) => {
+        monoid! { $type where []; one = $one; mul($self, $y) = $code; }
+    };
+}
+
+impl Monoid for i64 {
+    fn one() -> Self {
+        1
+    }
+}
+impl Monoid for i128 {
+    fn one() -> Self {
+        1
+    }
+}
+impl Monoid for f64 {
+    fn one() -> Self {
+        1.0
+    }
+}
+
+// @algebra/ring
+/// Algebra - Ring ((+, 0), (*, 1))
+pub trait Ring: AGroup + Monoid {}
+
+#[macro_export]
+macro_rules! ring {
+    (
+        $type:ty where [ $( $params:tt )* ];
+        div($self:ident, $other:ident) = $code:block
+        $(;)*
+    ) => {
+        impl<$($params)*> std::ops::Div for $type {
+            type Output = Self;
+            fn div($self, $other: Self) -> Self { $code }
+        }
+        impl<$($params)*> std::ops::DivAssign for $type where Self: Clone {
+            fn div_assign(&mut $self, $other: Self) { *$self = (*$self).clone() / $other; }
+        }
+        impl Ring for $type {}
+    };
+    (
+        $type:ty;
+        div($self:ident, $other:ident) = $code:block
+        $(;)*
+    ) => {
+        ring! { $type where []; div($self, $other) = $code; }
+    };
+}
+
+impl Ring for i64 {}
+impl Ring for f64 {}
+
+// {{{
+use std::io::{self, Write};
+use std::str::FromStr;
+#[derive(Default)]
+pub struct Scanner {
+    buffer: VecDeque<String>,
+}
+impl Scanner {
+    pub fn cin<T: FromStr>(&mut self) -> T {
+        while self.buffer.is_empty() {
+            let mut line = String::new();
+            let _ = io::stdin().read_line(&mut line);
+            self.buffer = line.split_whitespace().map(|w| String::from(w)).collect();
+        }
+        self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap()
+    }
+    pub fn usize1(&mut self) -> usize {
+        self.cin::<usize>() - 1
+    }
+    pub fn chars(&mut self) -> Vec<char> {
+        self.cin::<String>().chars().collect()
+    }
+    pub fn vec<T: FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.cin()).collect()
+    }
+    pub fn pair<S: FromStr, T: FromStr>(&mut self) -> (S, T) {
+        (self.cin::<S>(), self.cin::<T>())
+    }
+}
+fn flush() {
+    io::stdout().flush().unwrap();
+}
+#[macro_export]
+macro_rules! min {
+    (.. $x:expr) => {{
+        let mut it = $x.iter();
+        it.next().map(|z| it.fold(z, |x, y| min!(x, y)))
+    }};
+    ($x:expr) => ($x);
+    ($x:expr, $($ys:expr),* $(,)*) => {{
+        let t = min!($($ys),*);
+        if $x < t { $x } else { t }
+    }}
+}
+#[macro_export]
+macro_rules! max {
+    (.. $x:expr) => {{
+        let mut it = $x.iter();
+        it.next().map(|z| it.fold(z, |x, y| max!(x, y)))
+    }};
+    ($x:expr) => ($x);
+    ($x:expr, $($ys:expr),* $(,)*) => {{
+        let t = max!($($ys),*);
+        if $x > t { $x } else { t }
+    }}
+}
+#[macro_export]
+macro_rules! trace {
+    (# $a:ident $(,)? $(;)? $($xs:expr),* $(,)? ) => {
+        #[cfg(debug_assertions)]
+        eprintln!("[{}] {} = {:?}", stringify!($a), stringify!($($xs),*), ($($xs),*))
+    };
+    ($($xs:expr),* $(,)?) => {
+        #[cfg(debug_assertions)]
+        eprintln!(">>> {} = {:?}", stringify!($($xs),*), ($($xs),*))
+    };
+}
+#[macro_export]
+macro_rules! put {
+    (# $a:ident) => {println!("{}", stringify!($a))};
+    (.. $x:expr) => {{
+        let mut it = $x.iter();
+        if let Some(x) = it.next() { print!("{}", x); }
+        for x in it { print!(" {}", x); }
+        println!("");
+    }};
+    ($x:expr) => { println!("{}", $x) };
+    ($x:expr, $($xs:expr),*) => { print!("{} ", $x); put!($($xs),*) }
+}
+#[macro_export]
+macro_rules! ndarray {
+    ($x:expr;) => { $x };
+    ($x:expr; $size:expr $( , $rest:expr )*) => {
+        vec![ndarray!($x; $($rest),*); $size]
+    };
+}
+/// Array-indexing by i64.
+/// (Vec<Vec<..<T>..>>; i64, i64, ..) => Option<T>
+#[macro_export]
+macro_rules! at {
+    ($s:expr;) => { Some($s) };
+    ($s:expr; $idx:expr $(,$args:expr)* $(,)?) => {
+        if 0 <= $idx {
+            let idx_usize = $idx as usize;
+            if idx_usize < $s.len() {
+                at!($s[idx_usize]; $($args),*)
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+}
+// }}}
